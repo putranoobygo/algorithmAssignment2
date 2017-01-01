e615e3a1b473f58037401bd136ea5a170ded1633
Cage Spence <cage.spence@gmail.com>
2017-01-01 14:19:25 +0100

    everything works

--- src/Main.java
+++ src/Main.java
@@ -2,82 +2,33 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-        final int amountOfValues = 15;
-        final int heapSize = 5;
-        RSHeap heap = new RSHeap(new Main().generateValues(amountOfValues), heapSize);
+        new Main();
     }
 
     public Main() {
 
-    }
+        final int[] n = {10, 10, 5, 20, 0, 20, 1, 4, 16, 100};
+        final int[] h = {10, 5, 10, 0, 20, 1, 20, 16, 4, 3};
 
-    private void testHeap() {
-        MinHeap minHeap = new MinHeap(15);
-
-        minHeap.insert(0);
-        minHeap.insert(1);
-        minHeap.insert(2);
-        minHeap.insert(3);
-        minHeap.insert(4);
-        minHeap.insert(5);
-        minHeap.insert(6);
-        minHeap.insert(7);
-        minHeap.insertDead(10);
-        minHeap.insertDead(11);
-        minHeap.insert(8);
-        minHeap.insertDead(12);
-        minHeap.insertDead(13);
-        minHeap.insertDead(14);
-        minHeap.insertDead(15);
-        minHeap.insertDead(16); // err
-        minHeap.print(); // 9 last pos, 6 dead space, empty 0
-
-        minHeap.pop();
-        minHeap.print(); // 8 last pos, 6 dead space, empty 1 [9]
-        minHeap.pop();
-        minHeap.print(); // 7 last pos, 6 dead space, empty 2 [9]
-
-        minHeap.insertDead(16);
-        minHeap.insertDead(17);
-        minHeap.print(); // 7 last pos, 8 dead space, empty 0
-
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop();
-        minHeap.pop(); // err
-        minHeap.print(); // 0 last pos, 8 dead space, empty 7 [1]
-
-        minHeap.insertDead(18);
-        minHeap.insertDead(19);
-        minHeap.insertDead(20);
-        minHeap.insertDead(21);
-        minHeap.insertDead(22);
-        minHeap.insertDead(23);
-        minHeap.insertDead(25); // run finished, all dead space converted to usable space, heap still full
-        minHeap.insertDead(24); // err
-        minHeap.insert(2); // err
-        minHeap.print(); // last pos 15, dead space 0, empty 0
-
-        minHeap.printTrees();
-    }
-
-    public int[] generateValues(int n) {
-
-        int[] permutations = new int[n];
-        int swapPosition;
-        long counter = 0;
-        for (int i = 0; i < n; i++) {
-            counter++;
-            swapPosition = (int)(Math.random() * (i + 1)); //0-i
-            permutations[i] = permutations[swapPosition]; //replace [i] with the temp value
-            permutations[swapPosition] = i; //replace the [swapPosition] with i
+        for (int i = 0; i < n.length; i++) {
+            int[] inputFile = generateRandomList(n[i]);
+            RSHeap minHeap = new RSHeap(h[i]);
+            minHeap.processArray(inputFile);
         }
+    }
 
-        return permutations;
+    /**
+     * Generates a random list of numbers between zero and and a specified number
+     *
+     * @param n the maximum value range in the list
+     * @return a random list of integers
+     */
+    private int[] generateRandomList(int n) {
+        Random r = new Random();
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = r.nextInt(n);
+        }
+        return list;
     }
 }
--- src/RSHeap.java
+++ src/RSHeap.java
@@ -1,21 +1,301 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.nio.Buffer;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RSHeap {
 
-    //in this class we need to use the heap to sort the output runs
-    final private int heapSize;
-    final private MinHeap heap;
-    private int[] values;
+    private int[] heap;
+    private int maxSize;
+    private int heapSize;
+    private int deadSpace;
+    private int runLength;
+    private int outputPos;
+    private int[] output;
 
-    public RSHeap(int[] values, int heapSize){
-        this.values = values;
-        this.heapSize = heapSize;
-        heap = new MinHeap(heapSize);
-        heap.process(values);
+    private ArrayList<Integer> runLengths;
+
+    private static final int FRONT = 1;
+
+    /**
+     * Constructor initializes heap with maxSize, sets heapSize to
+     *
+     * @param maxSize maximum maxSize of the heap
+     */
+    public RSHeap(int maxSize) {
+        this.maxSize = maxSize;
+        heapSize = 0;
+        deadSpace = 0;
+        runLength = 0;
+        outputPos = 0;
+        runLengths = new ArrayList<>();
+        heap = new int[this.maxSize + 1];
+        heap[0] = -999;
     }
 
+    /**
+     * @param pos
+     * @return
+     */
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    /**
+     * Prints the parents and children
+     */
+    private void print() {
+        //heap, heapSize, deadspace, empty [from], outputPos
+//        System.out.println("\t\t"+Arrays.toString(Arrays.copyOfRange(heap,1,maxSize+1)));
+        System.out.println("\t\t" + toString(Arrays.copyOfRange(heap, 1, maxSize + 1)));
+        System.out.println(">>\t" + Arrays.toString(Arrays.copyOfRange(output, 0, outputPos)));
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+
+    /**
+     * Swaps two nodes
+     *
+     * @param fPos the first position
+     * @param sPos the second position
+     */
+    private void swap(int fPos, int sPos) {
+        int tmp;
+        tmp = heap[fPos];
+        heap[fPos] = heap[sPos];
+        heap[sPos] = tmp;
+    }
+
+    /**
+     * Inserts an element at the last position of the heap (!then applies percolate up to move the node to the correct position)
+     *
+     * @param element the element to be inserted
+     */
+    private void insert(int element) {
+        if (intSpaceAvailable() > 0) {
+            heap[++heapSize] = element;
+            System.out.println("\t\t> " + element);
+        } else {
+            System.out.println("- Error inserting element " + element + ", heap full");
+        }
+        int current = heapSize;
+        while (heap[current] < heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+        bubbleUp();
+    }
+
+    /**
+     * @param element
+     */
+    private void insertDead(int element) {
+        if (intSpaceAvailable() > 0) {
+            heap[maxSize - deadSpace] = element;
+            ++deadSpace;
+            System.out.println("\t\t> (" + element + ")");
+        } else {
+            System.out.println("- Error inserting dead element " + element + ", heap full");
+        }
+    }
+
+    /**
+     *
+     */
+    private void bubbleUp() {
+        int pos = heapSize - 1;
+        while (pos > 0 && heap[pos / 2] > heap[pos]) {
+            int y = heap[pos];
+            heap[pos] = heap[pos / 2];
+            heap[pos / 2] = y;
+            pos = pos / 2;
+        }
+    }
+
+    /**
+     * Removes the top element and re-heaps the tree
+     *
+     * @return top element
+     */
+    private int pop() {
+
+        int popped = heap[FRONT];
+
+        heap[FRONT] = heap[heapSize--];
+        sinkDown(FRONT);
+        System.out.println("\tpop " + popped);
+
+        return popped;
+    }
+
+    /**
+     * The specified element is compared with its children and swapped if necessary; any elements swapped also have the operation performed on them.
+     *
+     * @param k the element at which the sink down operation should be started from
+     */
+    private void sinkDown(int k) {
+        int smallest = k;
+        if (2 * k <= heapSize && heap[smallest] >= heap[2 * k]) {
+            smallest = 2 * k;
+        }
+        if (2 * k + 1 <= heapSize && heap[smallest] >= heap[2 * k + 1]) {
+            smallest = 2 * k + 1;
+        }
+        if (smallest != k) {
+            swap(k, smallest);
+            sinkDown(smallest);
+        }
+    }
+
+    /**
+     * Gets the top element of the heap
+     *
+     * @return top element
+     */
+    private int getPop() {
+        return heap[FRONT];
+    }
+
+    /**
+     * @return the number of free spaces in the data structure
+     */
+    private int intSpaceAvailable() {
+        return maxSize - deadSpace - heapSize;
+    }
+
+    /**
+     * @param input the input array to be processed
+     * @return
+     */
+    public int[] processArray(int[] input) {
+
+        System.out.println("processing array; N = " + input.length + ", H = " + maxSize);
+
+        if (maxSize == 0 || maxSize == 1 || input.length == 0) {
+            return input;
+        }
+
+        boolean endOfheap = false;
+
+        // reset run lengths
+        runLengths.clear();
+        runLength = 0;
+
+        // store position of input array cursor
+        int inputPos = 0;
+
+        // store the value of last output
+        int lastOutput = Integer.MIN_VALUE;
+
+        // create output array
+        output = new int[input.length];
+
+        // while output array not full
+        while (outputPos < input.length && !endOfheap) {
+
+            // check if the
+            endOfheap = (heapSize < 1 && deadSpace < 1 && outputPos > 1);
+
+            // space available to place elements in heap
+            while (intSpaceAvailable() > 0) {
+
+                System.out.println(intSpaceAvailable() + " free spaces");
+
+                // elements left in the input array
+                if (inputPos < input.length) {
+
+                    System.out.println(Arrays.toString(Arrays.copyOfRange(input, inputPos, input.length)) + "  <<");
+
+                    // insert element into heap or dead space
+                    if (input[inputPos] >= lastOutput) {
+                        insert(input[inputPos]);
+                    } else {
+                        insertDead(input[inputPos]);
+                    }
+
+                    // increment
+                    inputPos++;
+
+                    print();
+                } else break;
+            }
+
+            // start outputting
+
+            // if there are elements in heap
+            if (heapSize > 0) {
+                lastOutput = getPop();
+
+                // put top element from heap into output
+                output[outputPos] = pop();
+
+                // increment
+                outputPos++;
+                runLength++;
+
+                System.out.println("output " + outputPos + "/" + output.length);
+
+                print();
+            } else if (deadSpace > 0) {
+
+                System.out.println("converting dead space to heap");
+
+                // convert dead space into heap space
+                int dead[] = Arrays.copyOfRange(heap, heap.length - deadSpace, heap.length);
+                heapSize = 0;
+                deadSpace = 0;
+                for (int i = 0; i < dead.length; i++) {
+                    insert(dead[i]);
+                }
+                print();
+
+                // mark the end of the run
+                runLengths.add(runLength);
+                runLength = 0;
+            }
+        }
+        if (runLength > 0) {
+            runLengths.add(runLength);
+        }
+        System.out.println("runlengths " + runLengths);
+        return output;
+    }
+
+    /**
+     * @param array
+     * @return
+     */
+    public String toString(int[] array) {
+        if (array == null) {
+            return "null";
+        } else {
+            int var1 = array.length - 1;
+            if (var1 == -1) {
+                return "{}";
+            } else {
+                StringBuilder output = new StringBuilder();
+                output.append('{');
+                int i = 0;
+
+                while (true) {
+                    //here is where the numbers should be formatted
+                    if (i >= heapSize) {
+                        if (i < array.length - deadSpace) {
+                            output.append('-');
+                        } else {
+                            output.append('(');
+                            output.append(array[i]);
+                            output.append(')');
+                        }
+                    } else {
+                        output.append(array[i]);
+                    }
+                    //
+                    if (i == var1) {
+                        return output.append('}').toString();
+                    }
+
+                    output.append(", ");
+                    ++i;
+                }
+            }
+        }
+    }
 }
